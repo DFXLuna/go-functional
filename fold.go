83098520fe5ad6caf3f041dbaa31d0310424316f
Emilia <eg@dfxluna.xyz>
2023-07-08 12:31:23 -0600

    docs tune up

--- fold.go
+++ fold.go
@@ -11,7 +11,7 @@ var (
 )
 
 // Applies a right fold over an array of type U, summing the results into the accumulator of type T by repeatedly applying
-// function F to every member of us
+// function F to every member of us.
 func Foldr[T Summable, U any](acc T, f func(acc T, u U) T, us []U) T {
 	for _, u := range us {
 		acc += f(acc, u)
@@ -31,6 +31,8 @@ func FoldrE[T Summable, U any](acc T, f func(acc T, u U) (T, error), us []U) (T,
 	return acc, nil
 }
 
+// Applies a left fold over an array of type U, summing the results into the accumulator of type T by repeatedly applying
+// function F to every member of us. Equivalent to iteration from the end of an array to the beginning of an array
 func Foldl[T Summable, U any](acc T, f func(acc T, u U) T, us []U) T {
 	for i := len(us) - 1; i >= 0; i-- {
 		acc += f(acc, us[i])
@@ -38,6 +40,7 @@ func Foldl[T Summable, U any](acc T, f func(acc T, u U) T, us []U) T {
 	return acc
 }
 
+// Applies Foldl as above but allows function f to error and returns that error
 func FoldlE[T Summable, U any](acc T, f func(acc T, u U) (T, error), us []U) (T, error) {
 	for i := len(us) - 1; i >= 0; i-- {
 		v, err := f(acc, us[i])
--- map.go
+++ map.go
@@ -9,6 +9,8 @@ var (
 	ErrMap = errors.New("error appling map")
 )
 
+// Transform provided array ts into a new array with elements of
+// type U by applying function f to each element of ts and storing the result
 func Map[T any, U any](ts []T, f func(T) U) []U {
 	us := make([]U, 0, len(ts))
 	for _, t := range ts {
@@ -17,6 +19,7 @@ func Map[T any, U any](ts []T, f func(T) U) []U {
 	return us
 }
 
+// Same as Map but with the possibility of returning an error
 func MapE[T any, U any](ts []T, f func(T) (U, error)) ([]U, error) {
 	us := make([]U, 0, len(ts))
 	for _, t := range ts {
